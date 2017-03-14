d19ad0255b07777a2394e7c90c043ef3481b8c91
Simbadluca <a.luca@bluewin.ch>
2017-03-14 20:08:49 +0100

    correctiopns du 2

---
+++ src/01-about-ts-types/test.ts
@@ -0,0 +1,53 @@
+import * as chai from 'chai';
+var expect = chai.expect;
+
+describe('about ts types', () => {
+  it('1-should compile to plain javascript', () => {
+    var boolean=true; // _
+    var integer=6; // _
+    var float=Math.PI; // _
+    var foo='bar'; // _
+    expect(boolean).to.be.true;
+    expect(integer).to.equal(6);
+    expect(float).to.equal(Math.PI);
+    expect(foo).to.equal('bar');
+  });
+
+  it('2-should type arrays', () => {
+    var list='we'; // _
+    expect(list.length).to.equal(2);
+  });
+
+  it('3-should type tuples', () => {
+    var tupple=[42,'foo']; // _
+    expect(tupple[0]).to.equal(42);
+    expect(tupple[1]).to.equal('foo');
+  });
+
+  it('4-should type enums', () => {
+    enum myAwesomeEnum {'ACTIVE','dert', 'INACTIVE'};
+    expect(myAwesomeEnum.ACTIVE).to.equal(0);
+    expect(myAwesomeEnum[2]).to.equal('INACTIVE');
+  });
+
+  it('5-should type null and undefined', () => {
+    var nullVar=null;
+    var undefinedVar;
+    expect(nullVar).to.be.null;
+    expect(undefinedVar).to.be.undefined;
+  });
+
+  it('6-should work in functions arguments too', () => {
+    function sayHello(name) {
+      return 'Hello '.concat(name);
+    }
+    expect(sayHello('TypeScript')).to.equal( 'Hello TypeScript' );
+  });
+
+  it('7-should infer the type', () => {
+    function add(a, b) {
+      return a + b;
+    }
+    expect(add(17, '25')).to.equal( '1725' );
+  });
+});
